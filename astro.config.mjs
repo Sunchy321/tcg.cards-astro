34e8ddeca930ab31ad1a8f5f875c9aea8a7b58a8
Sunchy321 <sunchy321@qq.com>
2026-02-21 19:04:36 +0800

    feat: add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,20 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    build: {
-        inlineStylesheets: 'never',
-    },
-    output: 'server',
-    adapter: cloudflare({
-    }),
+  build: {
+      inlineStylesheets: 'never',
+  },
+
+  output: 'server',
+
+  adapter: cloudflare({
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
